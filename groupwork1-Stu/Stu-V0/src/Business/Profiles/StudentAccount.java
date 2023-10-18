7d3cafb885b79c94d9be7663c30164e1a20a3e37
ZJM0312 <zhang.jinmin@northeastern.edu>
2023-10-18 13:02:59 -0400

    zjm1

---
+++ groupwork1-Stu/Stu-V0/src/Business/Profiles/StudentAccount.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Profiles;
+
+import Business.Course.CourseDirectory;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class StudentAccount {
+    //存储一个人 
+    private CourseDirectory courseList;
+  
+    private String accountName;
+    private String password;
+}
